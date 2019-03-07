63f8fd0e0ddf67e5535c69563d930bd581f867ea
calumgiff1997 <46603690+calumgiff1997@users.noreply.github.com>
2019-03-07 12:52:20 +0000

    Allow this please

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeManager(models.Model):
+    employeeID = models.IntegerField(max_length=8, primary_key = True)
+    password = models.CharField(max_length=32)
+    
+class Car(models.Model):
+    registration = models.CharField(max_length=30, primary_key = True)
+    make/model = models.CharField(max_length=30)
+    issues = models.CharField(max_length=256)
+    
+class Contract(models.Model):
+    contractID = models.CharField(max_length=32, primary_key = True)
+    registration = models.CharField(max_length=16)
+    employeeID = models.IntegerField(max_length=8)
+    customerID = models.IntegerField(max_length=8)
+                           
+
+class Customer(models.Model):
+    customerID = models.IntegerField(max_length=8, primary_key = True)
+    first_name = models.CharField(max_length=16)
+    last_name = models.CharField(max_length=16)
+    email = models.CharField(max_length=32)
+# Create your models here.l,.
